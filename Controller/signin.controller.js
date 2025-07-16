5e4bb6b5e4a6f2bd31e7559e0868a954289eccc9
leanwithkumar <mrhustle936@gmail.com>
2025-07-16 11:09:22 +0530

    fixing error 6

--- Controller/signin.controller.js
+++ Controller/signin.controller.js
@@ -29,7 +29,6 @@ export const signin = async(req, res)=>{
         httpOnly: true,          
         sameSite: "None",    
         secure: true,  
-        domain: ".vercel.app",
         maxAge: 24 * 60 * 60 * 1000                    
         });
 
